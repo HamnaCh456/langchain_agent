9ef78038eb6c4038a8128ccc003f25ea9c733588
Hamna_Hameed <164740569+HamnaCh456@users.noreply.github.com>
2025-10-17 19:01:09 +0500

    initial commit

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 from langchain.chat_models import init_chat_model
 
@@ -9,3 +8,4 @@ model = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
 response=model.invoke([{"role": "user", "content": "What are AI Agents?"}]) 
 
 print(response.content)
+
