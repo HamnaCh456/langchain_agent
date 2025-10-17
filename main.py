4f939098f987d968b92ee64712d3b4103f44e26c
Hamna_Hameed <164740569+HamnaCh456@users.noreply.github.com>
2025-10-17 19:00:35 +0500

    Update user query in main.py to 'What are AI Agents?'commit

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 
 import os
+from langchain.chat_models import init_chat_model
 
 
 os.environ["GOOGLE_API_KEY"] = "YOUR_GOOGLE_API_KEY"
 
-from langchain.chat_models import init_chat_model
-
 model = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
-response=model.invoke([{"role": "user", "content": "Hello world!"}]) 
+response=model.invoke([{"role": "user", "content": "What are AI Agents?"}]) 
+
 print(response.content)
